0682e4defba0363c8c4711875b1b3f4c86ae3779
Anuraag8055 <anuraag.g8055@gmail.com>
2025-03-27 17:18:36 +0530

    jwt secret added

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,7 @@
-export default () => ({});
+export default ({ env }) => ({
+    "users-permissions": {
+      config: {
+        jwtSecret: env("JWT_SECRET", "21bd9247f0af2048128234f5a92eab522f38f2b0509542afc12600b31f6f7f13"),
+      },
+    },
+  });
